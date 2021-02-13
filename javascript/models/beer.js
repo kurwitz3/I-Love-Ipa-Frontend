b6c6e02b213a0d16043eeb33d4b6b823415c99c8
kurwitz3 <kurwitz3@aol.com>
2021-02-13 18:51:03 -0500

    added beer class and added html

---
+++ javascript/models/beer.js
@@ -0,0 +1,12 @@
+class Beer{
+    constructor({beer_name,beer_style,ibu,alcohol_percentage,
+        link,image,likes}){
+            this.beer_name = beer_name
+            this.beer_style = beer_style
+            this.ibu = ibu
+            this.alcohol_percentage = alcohol_percentage
+            this.link = link
+            this.image = image
+            this.likes = likes  
+    }
+}
